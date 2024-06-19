ce239b263e12e820dec471f113dc4a241f929124
Mike Wagstaff <mike.wagstaff@gmail.com>
2024-06-19 22:45:26 +0100

    only deploy scripts

--- train-track/deploy.sh
+++ train-track/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # For all scripts except this one, ensure all files are executable, and copy to the xbar plugins directory
-for file in *; do
+for file in *.sh; do
     if [ "$file" != "deploy.sh" ]; then
         echo "Deploying script: $file"
         chmod +x $file
